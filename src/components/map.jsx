6565420854b7eb91b7678e917b5da2184d1f3c56
shripraveen21 <103127975+shripraveen21@users.noreply.github.com>
2024-07-21 19:23:58 +0530

    marker.png

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,16 +3,14 @@ import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import './map.css';
+import markerIcon from './assets/marker.png';
 
 // Custom icon for markers
 const customIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: markerIcon,
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-  shadowSize: [41, 41],
 });
 
 const Map = () => {
